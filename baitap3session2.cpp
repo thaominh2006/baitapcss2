fc8bcba8ef62fd7c59d829abe396b7d117b25ae5
thaominh2006 <dothaominh2401@gmail.com>
2024-11-20 19:45:21 +0700

    Add files via upload

---
+++ baitap3session2.cpp
@@ -0,0 +1,14 @@
+   #include<stdio.h>
+   
+   int main(){
+   	
+   	   int number1=30;
+   	   int number2=50;
+   	   
+   	   int sum = number1 + number2;
+   	   int difference = number1 - number2;
+   	   int product = number1 * number2;
+   	   int quotient = number1 / number2;
+   	   
+   	   return 0;
+   }
---
+++ baitap5session2.cpp
@@ -0,0 +1,14 @@
+   #include<stdio.h>
+   
+   int main(){
+   	   float a = 20;
+   	   float b = 5;
+   	   
+   	   float perimetern = (a+b)/2;
+   	   float area = a * b;
+   	   
+   	   printf("Chu vi hinh chu nhat la %.2f\n",perimetern);
+   	   printf("Dien tich hinh chu nhat la %.2f\n",area);
+   	   
+   	   return 0;
+   }
