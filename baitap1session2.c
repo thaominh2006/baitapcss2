423f480dc4f648a674dcdfffbfec5b86dabff4a1
thaominh2006 <dothaominh2401@gmail.com>
2024-11-20 20:44:46 +0700

    Add files via upload

---
+++ baitap1session2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+	// Khai bao mot bien kieu int, dung de luu tru so nguyen; 
+	int a = 20; // Bien a luu tru gia tri 20;
+	
+	// Khai bao mot bien kieu float, dung de luu tru so thuc co phan thap phan; 
+	float b = 3.14; // Bien b luu tru gia tri 3.14;
+	
+	// Khai bao mot bien kieu double, dung de luu tru so thuc voi do chinh xac cao hon;
+	double c = 3.1415926535; // Bien c luu tru gia tri 3.1415926535;
+	
+	// Khai bao mot bien kieu char, dung de luu tru mot ky tu duy nhat; 
+	char d = 'A'; // Bien d luu tru gia tri 'A';
+	
+	// Khai bao mot bien kieu short, dung de luu tru so nguyen co pham vi nho hon int;
+	short e = 200; // Bien e luu tru gia tri 200; 
+	
+	// Khai bao mot bien kieu long, dung de luu tru so nguyen co pham vi lon hon int; 
+	long f = 200000; // Bien f luu tru gia tri 200000;
+	
+	// Khai bao mot bien kieu unsigned int, dung de luu tru so nguyen khong dau; 
+	unsigned int g = 70000; // Bien g luu tru gia tri 70000;
+	
+	return 0;
+}
+
+   
+	
+	
+ 
+	
---
+++ baitap2session2.c
@@ -0,0 +1,11 @@
+   #include<stdio.h>
+   
+   int main(){
+   	 
+   	   int number1=30;
+   	   int number2=50;
+   	   
+   	   int sum = number1 + number2;
+   	   
+   	   printf("Tong cua a va b la %d\n" ,sum);
+   }
